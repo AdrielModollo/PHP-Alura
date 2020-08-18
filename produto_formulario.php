a720515f6937b99a0e812a21706eab1c723faca1
Adriel Modollo <adrielmodollo@outlook.com>
2020-08-18 17:06:46 -0300

    Nova categoria com foreach

--- produto_formulario.php
+++ produto_formulario.php
@@ -1,4 +1,13 @@
-<?php include("cabecalho.php");?>
+<?php
+include("cabecalho.php");
+include("conecta.php");
+include("banco_produto.php");
+include("banco_categoria.php");
+
+
+$categorias = listaCategorias($conexao);
+?>
+
     <!--Faz um requisão para a página adiciona_produto -->
                 <h1>Formulario de produto</h1>
     <form action="adiciona_produto.php" method="post">
@@ -14,8 +23,16 @@
         <tr>
         <td>Descrição</td>
              <td> <textarea class= "form-control" name="descricao"> </textarea><br/></td>
+        </tr>
         <tr>
-        <td><input class= "btn btn-primary" type="submit" value="Cadastrar"></td>
+        <td> Categoria</td>
+        <td>
+            <?php foreach($categorias as $categoria) : ?>
+                <input type="radio" name="categoria_id" value="<?=$categoria['id']?>">  <?=$categoria['nome']?><br/>
+                                                        
+            <?php endforeach ?>
+        </td>
+            <td><input class= "btn btn-primary" type="submit" value="Cadastrar"></td>
         </tr>
 </table>
     </form>
