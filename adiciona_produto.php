7ddf117ce4e924a51ffff5bcd92f42d2477ca3a3
Adriel Modollo <adrielmodollo@outlook.com>
2020-08-17 22:06:22 -0300

    Estrutura e GET

---
+++ adiciona_produto.php
@@ -0,0 +1,7 @@
+<html>
+    <?php
+        $nome = $_GET["nome"];
+        $preco = $_GET["preco"];
+    ?>
+    <h1> Produto <?php echo $nome; ?> , <?php echo $preco; ?> adicionado com sucesso </h1>
+</html>
---
+++ index.php
@@ -0,0 +1,4 @@
+<html>
+    <!--Foi para o servidor fez a comunicação e retornou o valor abaixo -->
+     <h1>Bem vindo!</h1>
+</html>
