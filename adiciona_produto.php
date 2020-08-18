5d99dc69c408612898186c4be068de0ea32b705a
Adriel Modollo <adrielmodollo@outlook.com>
2020-08-18 10:10:08 -0300

    Adicionado uma function

--- adiciona_produto.php
+++ adiciona_produto.php
@@ -1,21 +1,29 @@
 <?php include("cabecalho.php");?>
     <?php
+        //Cria uma função que é chamada uma única vez
+        function insereProduto($conexao, $nome, $preco) {
+            //É informado parametros que o servidor vai receber
+            $query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+            //Retorna o resultado do mysql query
+            return mysqli_query($conexao, $query);
+        }
+
         /*Está é a resposta que foi solicitada através do produto_formulario */
         $nome = $_GET["nome"];
         $preco = $_GET["preco"];
+        //Conecta com o banco de dados MySql
         $conexao = mysqli_connect ('localhost', 'root', '', 'loja');
-
-        $query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
-        if (mysqli_query($conexao, $query)){ ?>
-            <p class="alert-success"> Produto <?php echo $nome; ?> , <?php echo $preco; ?> adicionado com sucesso </p>
+      
+       //Foi colocado uma condição para verificar se o produto foi adicionado ou não.
+       if (insereProduto($conexao, $nome, $preco)){ ?>
+            <!--     "alert-success" permite deixar o background verde -->
+            <p class="text-success"> Produto <?php echo $nome; ?> , <?php echo $preco; ?> adicionado com sucesso </p>
         <?php  } else { ?>
-            <p class="alert-danger"> Produto <?=  $nome ?>  não foi adicionado. </p>
+            <p class="text-danger"> Produto <?=  $nome ?>  não foi adicionado. </p>
         <?php
         
         }
 
        // mysqli_close($conexao);
-
- ?>   
-        
+ ?>      
  <?php include("rodape.php");?>
