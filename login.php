c043d43d5eb27bd2c2eff41af84bd5cafe1a6432
Adriel Modollo <adrielmodollo@outlook.com>
2020-08-19 14:47:46 -0300

    Adicionado login inicial a página

---
+++ login.php
@@ -0,0 +1,11 @@
+<?php
+include("conecta.php");
+include("banco_usuario.php");
+
+$usuario = buscaUsuario($conexao, $_POST["email"], $_POST["senha"]);
+if($usuario == null){
+    header ("Location: index.php?login=0");
+} else {
+    header("Location: index.php?login=1");
+}
+die();
